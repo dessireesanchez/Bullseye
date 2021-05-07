e5bb6dc0102b7bf2ea263d7645647fd4c3b696c9
Dessiree Sánchez <dessireesanchez@gmail.com>
2021-05-06 23:23:16 -0300

    Initial Commit

---
+++ Bullseye/BullseyeApp.swift
@@ -0,0 +1,17 @@
+//
+//  BullseyeApp.swift
+//  Bullseye
+//
+//  Created by Adrian De León on 5/6/21.
+//
+
+import SwiftUI
+
+@main
+struct BullseyeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
